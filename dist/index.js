b2c9da22ec45948d3373d9c1b96b0f94d83fdf9e
Carp-Popescu, Daniel <daniel.carp-popescu@dynatrace.com>
2023-08-22 16:36:40 +0200

    .

--- dist/index.js
+++ dist/index.js
@@ -2953,7 +2953,7 @@ const triggerWorkflow = async (inputs, accessToken) => {
             'Authorization': `Bearer ${accessToken}`,
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify(JSON.parse(inputs.payload)),
+        body: JSON.stringify(JSON.parse(JSON.stringify(inputs.payload))),
         method: 'POST',
     });
     const response = await request.json();
--- src/helper.ts
+++ src/helper.ts
@@ -96,7 +96,7 @@ export const triggerWorkflow = async (inputs: InputProps, accessToken: string): 
         'Authorization': `Bearer ${accessToken}`,
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(JSON.parse(inputs.payload!)),
+      body: JSON.stringify(JSON.parse(JSON.stringify(inputs.payload!))),
       method: 'POST',
     },
   );
